8a6d1513c4ca10713b41247ad2d470338c3dbdbf
HumbleGh <isaacoseianane@gmail.com>
2023-09-09 10:55:53 +0000

    minor changes

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -14,49 +14,75 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: PageView.builder(
-            itemCount: contents.length,
-            itemBuilder: (_, i) {
-              return Stack(
-                alignment: AlignmentDirectional.topEnd,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            alignment: Alignment.topCenter,
-                            image: AssetImage(
-                              contents[i].image,
-                            ),
-                            fit: BoxFit.fitWidth)),
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius:
-                              BorderRadius.vertical(top: Radius.circular(30))),
-                      height: 300,
-                      width: double.infinity,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: (20), horizontal: (20)),
-                        child: Column(
-                          children: [
-                            Text(
-                              contents[i].title,
-                              style: Styles.headLineStyle1,
-                            ),
-                            const Gap(10),
-                            Text(
-                              contents[i].description,
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
+      body: PageView.builder(
+        itemCount: contents.length,
+        itemBuilder: (_, i) {
+          return Stack(
+            alignment: AlignmentDirectional.topEnd,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        alignment: Alignment.topCenter,
+                        image: AssetImage(
+                          contents[i].image,
                         ),
-                      ),
+                        fit: BoxFit.fitWidth)),
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(30))),
+                  height: 300,
+                  width: double.infinity,
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: (20), horizontal: (20)),
+                    child: Column(
+                      children: [
+                        Text(
+                          contents[i].title,
+                          style: Styles.headLineStyle1,
+                        ),
+                        Gap(10),
+                        Text(
+                          contents[i].description,
+                          textAlign: TextAlign.center,
+                        ),
+                        Gap(60),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 20,
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Next',
+                                style: Styles.headLineStyle3
+                                    .copyWith(color: Colors.white),
+                              ),
+                              Gap(10),
+                              Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                ],
-              );
-            }));
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
