6a157a6812213f34201fc7bb485f912e5eb82cb5
HumbleGh <isaacoseianane@gmail.com>
2023-09-11 21:25:37 +0000

    Sign in page start

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:shortchanged/utils/app_style.dart';
 
 class SignInPage extends StatelessWidget {
@@ -26,6 +27,44 @@ class SignInPage extends StatelessWidget {
             style: Styles.headLineStyle4,
           ),
         ),
+        const Gap(20),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Enter your Mobile Number'),
+              const Gap(12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 0.5),
+                        borderRadius: BorderRadius.circular(8)),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    child: Row(
+                      children: [
+                        Image(image: AssetImage('assets/images/ghflag.png')),
+                        const Gap(6),
+                        Icon(Icons.arrow_drop_down),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 180,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'John',
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
       ])),
     );
   }
