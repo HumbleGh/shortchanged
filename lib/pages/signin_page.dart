e378cf26baa56ab3dc25d5b48a8537960b962405
HumbleGh <isaacoseianane@gmail.com>
2023-09-11 20:11:34 +0000

    Sign in page start

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:shortchanged/utils/app_style.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('This is the Sign in page')),
+    return Scaffold(
+      body: SafeArea(
+          child: ListView(children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+          width: 150,
+          height: 150,
+          child: const Image(image: AssetImage('assets/images/Logo.png')),
+        ),
+        Text(
+          'Sign in to your Account',
+          style: Styles.headLineStyle1,
+          textAlign: TextAlign.center,
+        ),
+        Center(
+          child: Text(
+            'Welcome back, please enter your details.',
+            style: Styles.headLineStyle4,
+          ),
+        ),
+      ])),
     );
   }
 }
