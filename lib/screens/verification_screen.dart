733a80814d67bb01a5083b3f76a5185fa0a4b836
HumbleGh <isaacoseianane@gmail.com>
2023-09-12 11:27:31 +0000

    verification screen copleted

--- lib/screens/verification_screen.dart
+++ lib/screens/verification_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:shortchanged/pages/signup_page.dart';
+import 'package:shortchanged/utils/app_style.dart';
 
 class VerificationScreen extends StatefulWidget {
   const VerificationScreen({super.key});
@@ -10,6 +13,118 @@ class VerificationScreen extends StatefulWidget {
 class _VerificationScreenState extends State<VerificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: Container(
+            padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 30.0),
+            child: SafeArea(
+                child: Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  padding: const EdgeInsets.symmetric(horizontal: 0),
+                  child: const Image(
+                    image: AssetImage('assets/images/Logo.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Text(
+                  'Verification',
+                  style: Styles.headLineStyle1,
+                  textAlign: TextAlign.center,
+                ),
+                Center(
+                  child: Text(
+                    'We\'ve sent an OTP to +233540977343',
+                    style: Styles.headLineStyle4,
+                  ),
+                ),
+                const Gap(40),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: 55,
+                        width: 55,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: const Text(' '),
+                      ),
+                      Container(
+                        height: 55,
+                        width: 55,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: const Text(' '),
+                      ),
+                      Container(
+                        height: 55,
+                        width: 55,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: const Text(' '),
+                      ),
+                      Container(
+                        height: 55,
+                        width: 55,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: const Text(' '),
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const VerificationScreen()));
+                    },
+                    child: const Text(
+                      'Continue',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Re-send code in '),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUpPage()));
+                      },
+                      child: const Text(
+                        '0:50',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ))));
   }
 }
