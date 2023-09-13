a216d1d3fd8e3fc01c66dc42c983cdf228d504b6
HumbleGh <isaacoseianane@gmail.com>
2023-09-13 23:28:01 +0000

    minor changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:shortchanged/utils/app_style.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,16 +19,36 @@ class _HomePageState extends State<HomePage> {
           Container(
             padding:
                 const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
               children: [
-                Text(
-                  'My Feed',
-                  style: Styles.headLineStyle1,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'My Feed',
+                      style: Styles.headLineStyle1,
+                    ),
+                    const Icon(
+                      Icons.notifications,
+                      color: Colors.black,
+                    )
+                  ],
                 ),
-                const Icon(
-                  Icons.notifications,
-                  color: Colors.black,
+                const Gap(20.0),
+                SingleChildScrollView(
+                  child: Row(children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 14, vertical: 13),
+                      decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(50)),
+                      child: const Text(
+                        'Sports',
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                      ),
+                    ),
+                  ]),
                 )
               ],
             ),
