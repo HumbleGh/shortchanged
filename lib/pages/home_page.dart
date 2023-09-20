14c861e9fa8fe02611c8f8e62b9578ff3b7e56c5
HumbleGh <isaacoseianane@gmail.com>
2023-09-20 08:41:07 +0000

    home page completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,162 +1,124 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
+import 'package:shortchanged/screens/coding_content.dart';
+import 'package:shortchanged/screens/education_content.dart';
+import 'package:shortchanged/screens/health_content.dart';
+import 'package:shortchanged/screens/politics_content.dart';
+import 'package:shortchanged/screens/sports_content.dart';
 import 'package:shortchanged/utils/app_style.dart';
-import 'package:shortchanged/widgets/category_text.dart';
-import 'package:shortchanged/widgets/double_text.dart';
-import 'package:shortchanged/widgets/large_news_card.dart';
-import 'package:shortchanged/widgets/medium_news_card.dart';
-import 'package:shortchanged/widgets/option_card.dart';
-import 'package:shortchanged/widgets/small_news_card.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  // ignore: library_private_types_in_public_api
+  _HomePage createState() => _HomePage();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePage extends State<HomePage> {
+  final List<String> tabTitles = [
+    "Sports",
+    "Coding",
+    "Education",
+    "Health",
+    "Politics",
+  ];
+  int _currentIndex = 0;
+
+  final List<Widget> tabContents = [
+    const SportsContent(),
+    const CodingContent(),
+    const EducationContent(),
+    const HealthContent(),
+    const PoliticsContent(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: ListView(
-        children: [
-          Container(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'My Feed',
-                      style: Styles.headLineStyle1,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'My Feed',
+                        style: Styles.headLineStyle1,
+                      ),
+                      const Icon(
+                        Icons.notifications,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 20),
+              height: 50.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: tabTitles.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _currentIndex = index;
+                      });
+                    },
+                    child: SizedBox(
+                      child: Container(
+                        margin: const EdgeInsets.only(left: 10.0),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0,
+                          vertical: 10.0,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50.0),
+                          color: _currentIndex == index
+                              ? Colors.blue
+                              : Colors.grey.shade200,
+                          border: Border.all(
+                            color: _currentIndex == index
+                                ? Colors.blue
+                                : Colors.grey.shade200,
+                            width: 1.0,
+                          ),
+                        ),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            tabTitles[index],
+                            style: TextStyle(
+                              color: _currentIndex == index
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                    const Icon(
-                      Icons.notifications,
-                      color: Colors.black,
-                    )
-                  ],
-                ),
-              ],
+                  );
+                },
+              ),
             ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Container(
-              padding: const EdgeInsets.only(left: 10),
-              child: Row(children: [
-                CategoryText(
-                  text: 'Sports',
-                  textColor: Colors.black,
-                  containerColor: Colors.grey.shade200,
-                ),
-                const CategoryText(
-                  text: 'Coding',
-                  textColor: Colors.white,
-                  containerColor: Colors.blue,
-                ),
-                CategoryText(
-                  text: 'Education',
-                  textColor: Colors.black,
-                  containerColor: Colors.grey.shade200,
-                ),
-                CategoryText(
-                  text: 'Health',
-                  textColor: Colors.black,
-                  containerColor: Colors.grey.shade200,
-                ),
-                CategoryText(
-                  text: 'Politics',
-                  textColor: Colors.black,
-                  containerColor: Colors.grey.shade200,
-                ),
-              ]),
+            Expanded(
+              child: Container(
+                color: Colors.grey.shade200,
+                child: tabContents[
+                    _currentIndex], // Display the selected content page
+              ),
             ),
-          ),
-          const Gap(20),
-          const DoubleText(bigText: 'Latest News', smallText: 'view all'),
-          const Gap(20),
-          const SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                MediumNewsCard(
-                  int: 1,
-                ),
-                MediumNewsCard(
-                  int: 2,
-                ),
-                MediumNewsCard(
-                  int: 3,
-                ),
-                MediumNewsCard(
-                  int: 4,
-                ),
-              ],
-            ),
-          ),
-          const Gap(20),
-          const DoubleText(bigText: 'Top Policies', smallText: 'view all'),
-          const Gap(14),
-          const LargeNewsCard(),
-          const Gap(20),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SmallNewsCard(int: 1),
-                SmallNewsCard(int: 2),
-              ],
-            ),
-          ),
-          const Gap(20),
-          const DoubleText(
-              bigText: 'Recently Added Polls', smallText: 'view all'),
-          const Gap(20),
-          Container(
-            // height: 240,
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            decoration: BoxDecoration(
-                color: Colors.grey[100],
-                border: Border.all(color: Colors.grey.shade400, width: 1),
-                borderRadius: BorderRadius.circular(8)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Should FREE SHS be tied to E-Levy',
-                      style: Styles.headLineStyle3,
-                    ),
-                    const Icon(
-                      Icons.more_horiz,
-                      color: Colors.grey,
-                    )
-                  ],
-                ),
-                const Gap(12),
-                const OptionCard(option: 'Yeah'),
-                const Gap(8),
-                const OptionCard(option: 'Not Really'),
-                const Gap(8),
-                const OptionCard(option: 'I don\'t care'),
-                const Gap(10),
-                const Text(
-                  '500 votes, 10 closes in 10 days',
-                  style: TextStyle(color: Colors.grey),
-                )
-              ],
-            ),
-          )
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/sports_content.dart
+++ lib/screens/sports_content.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:shortchanged/utils/app_style.dart';
 import 'package:shortchanged/widgets/double_text.dart';
 import 'package:shortchanged/widgets/large_news_card.dart';
 import 'package:shortchanged/widgets/medium_news_card.dart';
+import 'package:shortchanged/widgets/option_card.dart';
+import 'package:shortchanged/widgets/small_news_card.dart';
 
 class SportsContent extends StatefulWidget {
   const SportsContent({super.key});
@@ -16,8 +19,8 @@ class _SportsContentState extends State<SportsContent> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: const [
-          DoubleText(bigText: 'Latest News', smallText: 'view all'),
+        children: [
+          const DoubleText(bigText: 'Latest News', smallText: 'view all'),
           Gap(20),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
@@ -43,6 +46,58 @@ class _SportsContentState extends State<SportsContent> {
           Gap(14),
           LargeNewsCard(),
           Gap(20),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SmallNewsCard(int: 1),
+                SmallNewsCard(int: 2),
+              ],
+            ),
+          ),
+          const Gap(20),
+          const DoubleText(
+              bigText: 'Recently Added Polls', smallText: 'view all'),
+          const Gap(20),
+          Container(
+            // height: 240,
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            decoration: BoxDecoration(
+                color: Colors.grey[100],
+                border: Border.all(color: Colors.grey.shade400, width: 1),
+                borderRadius: BorderRadius.circular(8)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Should FREE SHS be tied to E-Levy',
+                      style: Styles.headLineStyle3,
+                    ),
+                    const Icon(
+                      Icons.more_horiz,
+                      color: Colors.grey,
+                    )
+                  ],
+                ),
+                Gap(12),
+                OptionCard(option: 'Yeah'),
+                Gap(8),
+                OptionCard(option: 'Not Really'),
+                Gap(8),
+                OptionCard(option: 'I don\'t care'),
+                Gap(10),
+                Text(
+                  '500 votes, 10 closes in 10 days',
+                  style: TextStyle(color: Colors.grey),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
