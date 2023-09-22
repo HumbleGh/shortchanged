63e51a074d475f7ecc4e6e4db0b11c7012864521
HumbleGh <isaacoseianane@gmail.com>
2023-09-22 10:31:19 +0000

    minor changes

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class NewsPage extends StatefulWidget {
-  const NewsPage({Key? key}) : super(key: key);
+  const NewsPage({super.key});
+
   @override
   State<NewsPage> createState() => _NewsPage();
 }
@@ -9,176 +10,11 @@ class NewsPage extends StatefulWidget {
 class _NewsPage extends State<NewsPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          body: DefaultTabController(
-        length: 5,
-        child: Column(
-          children: [
-            Material(
-              child: SizedBox(
-                height: 70,
-                // color: Colors.transparent,
-                child: TabBar(
-                  labelColor: Colors.white,
-                  physics: const BouncingScrollPhysics(),
-                  padding: const EdgeInsets.only(
-                      top: 10, left: 0, right: 0, bottom: 10),
-                  unselectedLabelColor: Colors.grey,
-                  isScrollable: true,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  indicator: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.blue),
-                  tabs: [
-                    Tab(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        height: 50,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            border: Border.all(color: Colors.blue, width: 1)),
-                        child: const Align(
-                          alignment: Alignment.center,
-                          child: Text("Chat"),
-                        ),
-                      ),
-                    ),
-                    Tab(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        height: 50,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            border: Border.all(color: Colors.blue, width: 1)),
-                        child: const Align(
-                          alignment: Alignment.center,
-                          child: Text("Status"),
-                        ),
-                      ),
-                    ),
-                    Tab(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        height: 50,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            border: Border.all(color: Colors.blue, width: 1)),
-                        child: const Align(
-                          alignment: Alignment.center,
-                          child: Text("Call"),
-                        ),
-                      ),
-                    ),
-                    Tab(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        height: 50,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            border: Border.all(color: Colors.blue, width: 1)),
-                        child: const Align(
-                          alignment: Alignment.center,
-                          child: Text("Call"),
-                        ),
-                      ),
-                    ),
-                    Tab(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        height: 50,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            border: Border.all(color: Colors.blue, width: 1)),
-                        child: const Align(
-                          alignment: Alignment.center,
-                          child: Text("Call"),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  ListView.separated(
-                    padding: const EdgeInsets.all(15),
-                    itemCount: 20,
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        onTap: () {},
-                        title: Text("Chat List $index"),
-                        subtitle: const Text("Tab bar ui"),
-                        trailing: const Icon(Icons.arrow_circle_right_sharp),
-                      );
-                    },
-                  ),
-                  ListView.separated(
-                    padding: const EdgeInsets.all(15),
-                    itemCount: 20,
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        onTap: () {},
-                        title: Text("Chat List $index"),
-                        subtitle: const Text("Tab bar ui"),
-                        trailing: const Icon(Icons.arrow_circle_right_sharp),
-                      );
-                    },
-                  ),
-                  ListView.separated(
-                    padding: const EdgeInsets.all(15),
-                    itemCount: 20,
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        onTap: () {},
-                        title: Text("Chat List $index"),
-                        subtitle: const Text("Tab bar ui"),
-                        trailing: const Icon(Icons.arrow_circle_right_sharp),
-                      );
-                    },
-                  ),
-                  ListView.separated(
-                    padding: const EdgeInsets.all(15),
-                    itemCount: 20,
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        onTap: () {},
-                        title: Text("Status List $index"),
-                        subtitle: const Text("Tab bar ui"),
-                        trailing: const Icon(Icons.arrow_circle_right_sharp),
-                      );
-                    },
-                  ),
-                  ListView.separated(
-                    padding: const EdgeInsets.all(15),
-                    itemCount: 20,
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        onTap: () {},
-                        title: Text("Call List $index"),
-                        subtitle: const Text("Tab bar ui"),
-                        trailing: const Icon(Icons.arrow_circle_right_sharp),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      )),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News Page'),
+      ),
+      body: const Center(child: Text('This is the News Page')),
     );
   }
 }
