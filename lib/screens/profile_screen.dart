0dc14056bfd1ae38fa469706bcd0cd5ac43d9ab6
HumbleGh <isaacoseianane@gmail.com>
2023-09-26 19:46:47 +0000

    profile screen started

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shortchanged/utils/app_layout.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -14,6 +16,35 @@ class _ProfileScreenState extends State<ProfileScreen> {
       appBar: AppBar(
         title: const Text('Profile'),
       ),
+      body: Column(children: [
+        Center(
+          child: Stack(
+            children: [
+              CircleAvatar(
+                radius: AppLayout.getHeight(60),
+                backgroundImage: const AssetImage('assets/images/man.png'),
+              ),
+              Positioned(
+                top: AppLayout.getHeight(60 + 20),
+                left: AppLayout.getHeight(60 + 20),
+                child: Container(
+                  width: AppLayout.getHeight(40),
+                  height: AppLayout.getHeight(40),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.grey.shade100),
+                  child: SvgPicture.asset(
+                    'assets/icons/Camera.svg',
+                    height: 10,
+                    width: 10,
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
