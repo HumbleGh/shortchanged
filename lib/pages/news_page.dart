40cbb90a8d6bd09ca14f1f9d5dd33ecdff7f574a
HumbleGh <isaacoseianane@gmail.com>
2023-09-28 17:47:02 +0000

    minor changes

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
+import 'package:shortchanged/pages/notification_page.dart';
 import 'package:shortchanged/screens/news_screen.dart';
+import 'package:shortchanged/utils/app_layout.dart';
 import 'package:shortchanged/widgets/large_news_card.dart';
 import 'package:shortchanged/widgets/small_news_card.dart';
-import 'package:shortchanged/widgets/title_notification.dart';
 
 class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
@@ -16,13 +18,33 @@ class _NewsPageState extends State<NewsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('News'), centerTitle: false, actions: [
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: ((context) => const NotificationPage()),
+              ),
+            );
+          },
+          child: Container(
+            height: 24,
+            width: 24,
+            margin: EdgeInsets.only(right: AppLayout.getHeight(20)),
+            child: SvgPicture.asset(
+              'assets/icons/Notification.svg',
+              height: 18,
+            ),
+          ),
+        )
+      ]),
       body: ListView(
         children: [
           Column(
             children: [
               Container(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: const TitleNotification(title: 'News'),
               ),
               Container(
                 height: 47,
@@ -35,11 +57,17 @@ class _NewsPageState extends State<NewsPage> {
                     color: Colors.grey.shade100,
                     borderRadius: BorderRadius.circular(12),
                     border: Border.all(width: 1, color: Colors.grey.shade400)),
-                child: const Row(children: [
-                  Icon(Icons.search),
-                  Gap(8),
-                  Text('Serach for news')
-                ]),
+                child: TextField(
+                    cursorColor: const Color(0x3C3D4000),
+                    decoration: InputDecoration(
+                      icon: SvgPicture.asset(
+                        'assets/icons/search-normal.svg',
+                        height: AppLayout.getHeight(18),
+                        width: AppLayout.getHeight(18),
+                      ),
+                      border: InputBorder.none,
+                      hintText: 'Search for news',
+                    )),
               ),
               GestureDetector(
                   onTap: () {
@@ -49,7 +77,7 @@ class _NewsPageState extends State<NewsPage> {
                             builder: (context) => const NewsScreen()));
                   },
                   child: const LargeNewsCard()),
-              const Gap(20),
+              Gap(AppLayout.getHeight(20)),
               Container(
                 width: double.infinity,
                 padding: const EdgeInsets.symmetric(horizontal: 20),
