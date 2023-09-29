3767dfd9b06ce7595563499100da549bc19ca4b0
HumbleGh <isaacoseianane@gmail.com>
2023-09-29 03:07:38 +0000

    comment feature implemented

---
+++ lib/widgets/comment.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:shortchanged/utils/app_layout.dart';
+import 'package:shortchanged/utils/app_style.dart';
+
+class Comment extends StatelessWidget {
+  const Comment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+      padding: EdgeInsets.symmetric(
+          horizontal: AppLayout.getHeight(20),
+          vertical: AppLayout.getHeight(20)),
+      height: 300,
+      decoration: BoxDecoration(
+          border: Border.all(
+              width: AppLayout.getHeight(1.0), color: Colors.grey.shade300),
+          borderRadius: BorderRadius.circular(
+            AppLayout.getHeight(10.0),
+          )),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 20,
+                  ),
+                  Gap(AppLayout.getHeight(10)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Jenny Wilson',
+                        style: Styles.headLineStyle3,
+                      ),
+                      const Text('3 days ago')
+                    ],
+                  )
+                ],
+              ),
+              const Icon(Icons.more_vert)
+            ],
+          ),
+          Gap(AppLayout.getHeight(10)),
+          Divider(
+            color: Colors.grey.shade300,
+          ),
+          Gap(AppLayout.getHeight(10)),
+          const Expanded(
+            child: Text(
+                'Lorem ipsum dolor sit amet, consecteur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.'),
+          ),
+          Gap(AppLayout.getHeight(10)),
+          Row(
+            children: [
+              SvgPicture.asset(
+                'assets/icons/like.svg',
+                height: 18,
+                // ignore: deprecated_member_use
+                color: Colors.blue,
+              ),
+              const Gap(6),
+              const Text('456'),
+              Gap(AppLayout.getHeight(10)),
+              SvgPicture.asset(
+                'assets/icons/dislike.svg',
+                height: 18,
+                // ignore: deprecated_member_use
+                color: Colors.red,
+              ),
+              const Gap(6),
+              const Text('456'),
+              Gap(AppLayout.getHeight(10)),
+              SvgPicture.asset(
+                'assets/icons/Chat.svg',
+                height: 18,
+                // ignore: deprecated_member_use
+                color: Colors.blue,
+              ),
+              const Gap(6),
+              const Text('456'),
+            ],
+          ),
+          Gap(AppLayout.getHeight(10)),
+          Divider(
+            color: Colors.grey.shade300,
+          ),
+          Gap(AppLayout.getHeight(10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('1876 Reply'),
+              SvgPicture.asset(
+                'assets/icons/Vector.svg',
+                height: 10,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:shortchanged/utils/app_layout.dart';
+import 'package:shortchanged/widgets/comment.dart';
 
 class CommentCard extends StatelessWidget {
   const CommentCard({super.key});
@@ -11,69 +12,102 @@ class CommentCard extends StatelessWidget {
     return GestureDetector(
         onTap: () {
           showModalBottomSheet(
-              backgroundColor: Colors.white,
               context: context,
               builder: (BuildContext context) {
                 return SizedBox(
-                  height: 600,
+                  height: 800,
                   child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: AppLayout.getHeight(20),
-                        vertical: AppLayout.getHeight(20)),
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(20)),
                     child: Column(
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                const Text(
-                                  'Comments',
-                                ),
-                                Gap(AppLayout.getHeight(6)),
-                                const Text(
-                                  '170',
-                                  style: TextStyle(color: Colors.blue),
-                                )
-                              ],
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.pop(context);
-                              },
-                              child: const Icon(
-                                Icons.close,
-                                color: Colors.blue,
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: AppLayout.getHeight(20)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  const Text(
+                                    'Comments',
+                                  ),
+                                  Gap(AppLayout.getHeight(6)),
+                                  const Text(
+                                    '170',
+                                    style: TextStyle(color: Colors.blue),
+                                  )
+                                ],
                               ),
-                            )
-                          ],
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Icon(
+                                  Icons.close,
+                                  color: Colors.blue,
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                        Gap(AppLayout.getHeight(10)),
+                        Gap(AppLayout.getHeight(5.0)),
                         const Divider(),
                         Gap(AppLayout.getHeight(20)),
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 20,
-                              child: Image.asset('assets/images/Profile.png'),
-                            ),
-                            Gap(AppLayout.getHeight(14)),
-                            Expanded(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  border: Border.all(
-                                      width: 1, color: Colors.grey.shade300),
-                                  color: Colors.white,
-                                ),
-                                height: 50,
-                                child: const TextField(
-                                  decoration:
-                                      InputDecoration(border: InputBorder.none),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: AppLayout.getHeight(20)),
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 20,
+                                child: Image.asset('assets/images/Profile.png'),
+                              ),
+                              Gap(AppLayout.getHeight(14)),
+                              Expanded(
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: AppLayout.getHeight(20)),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    border: Border.all(
+                                        width: 1, color: Colors.grey.shade300),
+                                    color: Colors.transparent,
+                                  ),
+                                  height: 50,
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: 'Add a comment...',
+                                        hintStyle: TextStyle(
+                                            color: Colors.grey.shade300)),
+                                  ),
                                 ),
                               ),
-                            )
-                          ],
+                            ],
+                          ),
+                        ),
+                        Gap(AppLayout.getHeight(20)),
+                        const Expanded(
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: Column(
+                              children: [
+                                Comment(),
+                                Gap(20),
+                                Comment(),
+                                Gap(20),
+                                Comment(),
+                                Gap(20),
+                                Comment(),
+                                Gap(20),
+                                Comment(),
+                                Gap(20),
+                                Comment(),
+                                Gap(20),
+                              ],
+                            ),
+                          ),
                         )
                       ],
                     ),
