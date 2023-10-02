3ea0925620330926cfbefe14d05434322106d6a2
HumbleGh <isaacoseianane@gmail.com>
2023-10-02 23:48:15 +0000

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       // Use GetMaterialApp instead of MaterialApp
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Shortchanged',
       theme: ThemeData(
         useMaterial3: true,
       ),
