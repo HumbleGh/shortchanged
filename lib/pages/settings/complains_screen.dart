2672e5f6e73c6fd6e900b293a378fa58a50f8c18
HumbleGh <isaacoseianane@gmail.com>
2023-10-13 23:55:23 +0000

    minor changes

--- lib/pages/settings/complains_screen.dart
+++ lib/pages/settings/complains_screen.dart
@@ -5,63 +5,6 @@ class ComplainsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Complaints & Suggestions'),
-        ),
-        body: Column(
-          children: [
-            Material(
-              child: Container(
-                color: Colors.white,
-                height: 70,
-                width: double.infinity,
-                child: TabBar(
-                    physics: const ClampingScrollPhysics(),
-                    padding: const EdgeInsets.only(
-                        top: 10, left: 10, right: 10, bottom: 10),
-                    unselectedLabelColor: Colors.blue,
-                    indicatorSize: TabBarIndicatorSize.label,
-                    indicator: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.blue),
-                    tabs: [
-                      Tab(
-                        child: Container(
-                          height: 70,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: const Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              "Complaints",
-                            ),
-                          ),
-                        ),
-                      ),
-                      Tab(
-                        child: Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: const Align(
-                            alignment: Alignment.center,
-                            child: Text("Suggestions"),
-                          ),
-                        ),
-                      ),
-                    ]),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
