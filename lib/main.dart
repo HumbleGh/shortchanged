56fbad76854aa02cdf3f2f4b3cdec8e9a700b7f9
HumbleGh <isaacoseianane@gmail.com>
2023-10-23 09:13:05 +0000

    code refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:shortchanged/screens/splash_screen.dart';
+import 'package:sizer/sizer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,14 +21,16 @@ class Shortchanged extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      // Use GetMaterialApp instead of MaterialApp
-      debugShowCheckedModeBanner: false,
-      title: 'Shortchanged',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const SplashScreen(),
-    );
+    return Sizer(builder: (context, Orientation, deviceType) {
+      return (GetMaterialApp(
+        // Use GetMaterialApp instead of MaterialApp
+        debugShowCheckedModeBanner: false,
+        title: 'Shortchanged',
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        home: const SplashScreen(),
+      ));
+    });
   }
 }
--- lib/screens/new_screen.dart
+++ lib/screens/new_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:sizer/sizer.dart';
 
 class NewScreen extends StatefulWidget {
   const NewScreen({super.key});
@@ -10,6 +12,21 @@ class NewScreen extends StatefulWidget {
 class _NewScreenState extends State<NewScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Stack(
+        children: [
+          Container(
+            height: 70.h,
+            decoration: BoxDecoration(color: Colors.amber),
+          ),
+          Container(
+            alignment: Alignment(50, 30),
+            height: 30.h,
+            decoration: BoxDecoration(color: Colors.blue),
+          )
+        ],
+      ),
+    );
   }
 }
